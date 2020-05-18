53d484ff1904b4dd9d89abd18da7e424c9b95e05
ATOM49 <abhilashmirji1@gmail.com>
2020-05-18 16:55:09 +0530

    initial commit added material ui redux typescript boilerplate

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 import { App as TodoApp } from 'app/containers/App';
+import { App as Home } from 'app/containers/Home';
+
 import { hot } from 'react-hot-loader';
 
 export const App = hot(module)(() => (
   <Switch>
-    <Route path="/" component={TodoApp} />
+    <Route path="/" component={Home} />
+    <Route path="/todo" component={TodoApp} />
   </Switch>
 ));
